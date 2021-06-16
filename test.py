894feb6b4ec67293f016cb1be3a209ab5d072169
Dhruvitt <pokharna.dhruvit@gmail.com>
2021-06-16 16:36:48 +0530

    remove tx_id and parents row

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ sample = open('samplefile.txt', 'w')
 with open('mempool.csv', 'r') as file:
     reader = csv.reader(file)
     for column in reader:
-        print(column[3], file = sample)
-        print(column[0], file = sample)
+        for row in reader:
+            print(row[3], file = sample)
+            print(row[0], file = sample)
 sample.close
